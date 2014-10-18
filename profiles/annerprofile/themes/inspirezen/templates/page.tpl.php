3ff105ffa3326673749fe27247e1dcbcf9addea5
Eoin Kilfeather <ekilfeather@gmail.com>
2014-10-18 14:03:06 +0100

    make titles translate

--- profiles/annerprofile/themes/inspirezen/templates/page.tpl.php
+++ profiles/annerprofile/themes/inspirezen/templates/page.tpl.php
@@ -72,7 +72,11 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <h1 class="page__title title" id="page-title">  
+        <?php if ($title): ?>
+    		<?php print t($title); ?>
+  		<?php endif; ?>
+  		</h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
