6cb748f81e890772a68a345dd47b1c61cf03b7bf
Eoin Kilfeather <ekilfeather@gmail.com>
2015-06-08 12:13:13 +0100

    contrib module security updates

--- profiles/annerprofile/modules/custom/dri_object/dri-iframe.tpl.php
+++ profiles/annerprofile/modules/custom/dri_object/dri-iframe.tpl.php
@@ -13,7 +13,7 @@
 <div class="iframe-object-wrapper">
   <div class="iframe-object-image"><?php print $colorbox_image; ?></div>
   <div class="iframe-object-inner">
-    <div class="iframe-object-title"><?php print $title; ?></div>
+    <div class="iframe-object-title"><?php print $title; ?>!!!!!</div>
     <div class="iframe-object-label"><?php print t('Creator:'); ?></div>
     <div class="iframe-object-creator"><?php print $creator; ?></div>
     <?php if ($date): ?>
@@ -21,6 +21,7 @@
       <div class="iframe-object-date"><?php print $date; ?></div>
     <?php endif; ?>
     <div class="iframe-object-description"><?php print $description; ?></div>
-    <div class="iframe-object-link"><a target="_top" href="/object-detail/<?php print $variables['elements']['0']['pid']; ?>"><?php print t('See Object Details'); ?></a></div>
+    <div class="iframe-object-link"><a target="_top" href="/object-detail/<?php print $variables['elements']['0']['pid']; ?>"><?php print t('See Object Details!!!'); ?></a></div>
+    <div class="iframe-object-link"><a target="_blank" href="http://dri.dmc.dit.ie/catalog/<?php print $variables['elements']['0']['pid']; ?>"><?php print t('See Object On DRI'); ?></a></div>
   </div>
 </div>
